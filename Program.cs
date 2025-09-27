069f6d58b863e6a2551471274a2063355515fc28
nicolee <nicolemathiasdasilva@gmail.com>
2025-09-26 21:17:50 -0300

    FINALIZAR

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("DIGITE UMA FRASE : ");
+string frase = Console.ReadLine();
+string frasec = frase 
+.Replace ("r" , "l") 
+.Replace ("R" , "L") ;
+
+Console.WriteLine(frasec);
